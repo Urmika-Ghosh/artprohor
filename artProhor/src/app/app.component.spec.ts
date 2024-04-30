79b80986cd4dd6a896f64d0f3978b39dfe85c675
subho-roy <subhodiproy25@gmail.com>
2024-04-30 15:42:33 +0530

    angular version changed

--- artProhor/src/app/app.component.spec.ts
+++ artProhor/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'artProhor'`, () => {
+  it(`should have the 'artProhor' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('artProhor');
@@ -30,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('artProhor app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, artProhor');
   });
 });
