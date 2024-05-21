900f64caed3a862b5a4d7048bb50ac40e0a8777f
subho-roy <subhodiproy25@gmail.com>
2024-05-22 00:02:26 +0530

    firestore installed

--- artProhor/src/app/app-routing.module.ts
+++ artProhor/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './Components/Auth/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/Auth/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo:'login',pathMatch:'full'
+  },
+  {
+    path:'login',component:SignInComponent
+  },
+  {
+    path:'register',component:SignUpComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
