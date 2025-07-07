9c0dcdc63a5b0c290b1538a5ad8c90e81d741abb
Urmika Ghosh <urmikaghosh@Urmikas-MacBook-Air.local>
2025-07-07 11:26:03 +0530

    signup page created

--- ART_PROHAR/src/app/app.routes.ts
+++ ART_PROHAR/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: '/home'},
+    {path: 'home', component: HomeComponent},
+      { path: 'signup', component: SignupComponent },
+];
